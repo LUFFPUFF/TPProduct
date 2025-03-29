249e329be3298d66d5db3bbd73b396ff9d618314
LUFFPUFF <beatsluffi@gmail.com>
2025-03-29 12:40:59 +0300

    fixed branch

--- Backend/src/main/java/com/example/model/company_subscription_module/user_roles/role/RoleName.java
+++
@@ -1,6 +0,0 @@
-package com.example.model.company_subscription_module.user_roles.role;
-
-public enum RoleName {
-
-    MANAGER, OPERATOR
-}
--- Backend/src/main/java/com/example/repository/company_subscription_module/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository.company_subscription_module;
-
-import com.example.model.company_subscription_module.user_roles.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
