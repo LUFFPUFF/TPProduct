6742659c55e070d523e475f1440c7018c1cda1a0
Sdr3nko <igorsidorenko632@gmail.com>
2025-05-02 14:36:19 +0300

    DX-115 fixed vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
