46e32b8490888e30510517f80a52e2aa20066f98
Sdr3nko <igorsidorenko632@gmail.com>
2025-05-02 14:25:50 +0300

    DX-115 change vite version

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,8 +10,9 @@ import tailwindcss from "@tailwindcss/vite"
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true,
+    host: '0.0.0.0',
+    port: 5173,
+    origin: 'http://dialogx.ru',
     cors: true,
-    origin: 'http://dialogx.ru'
   }
 })
