feae1bce3a4b31fbce4a78847fb04d0df08d04b9
C0r1e0ne <sahasid89@mail.ru>
2025-05-25 03:28:34 +0300

    DX-136 добавил отображение действующей подписки

--- frontend/src/pages/SubscriptionsPage.jsx
+++ frontend/src/pages/SubscriptionsPage.jsx
@@ -28,12 +28,14 @@ export default function SubscriptionsPage() {
             console.error("Ошибка при получении подписки:", error);
         }
     };
+
     useEffect(() => {
         if (subscriptionInfo) {
             console.log("endSubscription value:", subscriptionInfo?.end_subscription);
             console.log("Subscription Info обновлён:", subscriptionInfo);
         }
     }, [subscriptionInfo]);
+
     const fetchSoloPrice = async (months) => {
         try {
             const url = `${API.subscriptions.price}?months_count=${months}&operators_count=1`;
@@ -192,7 +194,7 @@ export default function SubscriptionsPage() {
                             max="500"
                             value={teamUsers}
                             onChange={(e) => {
-                                const value = Math.max(2, parseInt(e.target.value) || 2);
+                                const value = Math.min(1000, Math.max(2, parseInt(e.target.value) || 2));
                                 setTeamUsers(value);
                             }}
                             className="mt-1 mb-4 p-2 border border-gray-300 rounded w-full"
@@ -213,7 +215,7 @@ export default function SubscriptionsPage() {
                     aria-label="Открыть меню"
                 >
                     <svg className="w-8 h-8" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16M4 18h16"/>
                     </svg>
                 </button>
             </div>
@@ -237,71 +239,102 @@ export default function SubscriptionsPage() {
                                 className="text-gray-600 hover:text-black"
                                 aria-label="Закрыть меню"
                             >
-                                <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24">
-                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                <svg className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2"
+                                     viewBox="0 0 24 24">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"/>
                                 </svg>
                             </button>
                         </div>
                     </div>
                 </>
             )}
-
             <div className="flex-1 px-6 py-8 sm:px-10 sm:py-10 overflow-y-auto">
+                {subscriptionInfo?.status !== "ACTIVE" && (
+                    <>
+                        <h1 className="text-2xl sm:text-3xl font-bold text-black mb-4 sm:mb-6">
+                            Выберите подходящий тариф
+                        </h1>
+                        <p className="text-black text-base sm:text-lg mb-10">
+                            Гибкие тарифные планы для любого бизнеса. Экономьте с увеличением команды
+                        </p>
+
+                        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 sm:gap-8 justify-center items-stretch">
+                            {plans.map((plan, index) => (
+                                <div
+                                    key={index}
+                                    className="bg-white border-2 border-black rounded-lg shadow-[14px_14px_15px_rgba(0,0,0,0.32)] px-6 py-8 w-full sm:w-[90%] md:w-[420px] flex flex-col justify-between mx-auto"
+                                >
+                                    <div>
+                                        <h2 className="text-lg sm:text-xl font-extrabold text-black mb-1">
+                                            {plan.title}
+                                        </h2>
+                                        <p className="text-black font-medium mb-4">{plan.subtitle}</p>
+
+                                        {plan.durationInput}
+
+                                        <p className="text-xl sm:text-2xl font-bold text-black mb-4">
+                                            {plan.price}
+                                        </p>
+
+                                        <ul className="mb-6 space-y-2 text-black font-medium">
+                                            {plan.features.map((feature, i) => (
+                                                <li key={i}>• {feature}</li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                    <button
+                                        onClick={plan.onClick}
+                                        className="mt-auto bg-[#092155] text-white py-2 px-4 rounded hover:bg-[#2a4992] active:bg-[#dadee7] font-semibold active:text-black transition-all duration-150 ease-in-out transform active:scale-95">
+                                        {plan.button}
+                                    </button>
+                                </div>
+                            ))}
+                        </div>
+                    </>
+                )}
                 {subscriptionInfo?.status === "ACTIVE" && (
-                    <div className="text-black text-base sm:text-lg mb-6">
-                        Подписка активна до{" "}
-                        <strong>
-                            {subscriptionInfo.end_subscription
-                                ? new Date(subscriptionInfo.end_subscription).toLocaleString("ru-RU", {
-                                    day: "2-digit",
-                                    month: "long",
-                                    year: "numeric",
-                                    hour: "2-digit",
-                                    minute: "2-digit",
-                                })
-                                : "Дата недоступна"}
-                        </strong>
+                    <div className="flex flex-col">
+                        <h1 className="text-2xl sm:text-3xl font-bold text-left text-black mb-4 sm:mb-5">Подписка</h1>
+                        <div className="bg-white rounded-lg shadow-[14px_14px_15px_rgba(0,0,0,0.32)] p-4 sm:p-8 w-full overflow-hidden">
+                            <h2 className="text-xl sm:text-2xl font-bold mb-4">Данные о подписке</h2>
+                            <div className="mb-6">
+                                <p className="text-base sm:text-lg mb-2">Количество пользователей в подписке: {subscriptionInfo?.operators_count || 'Загрузка...'}</p>
+                                <p className="text-base sm:text-lg mb-6">Подписка действует до: {subscriptionInfo?.end_subscription ? new Date(subscriptionInfo.end_subscription).toLocaleDateString() : 'Загрузка...'}</p>
+                            </div>
+                            <div className="border-2 border-black rounded-lg p-4 sm:p-6 bg-gray-100">
+                                <h3 className="text-lg sm:text-xl font-bold mb-4 sm:mb-6 text-center">Управление</h3>
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 items-center">
+                                    <label className="text-sm sm:text-base font-medium md:col-span-1">Продлить подписку</label>
+                                    <input
+                                        type="number"
+                                        min="1"
+                                        placeholder="Введите срок в месяцах"
+                                        className="mt-1 p-2 bg-white border border-black rounded w-full md:col-span-2"
+                                    />
+                                </div>
+                                <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6 items-center">
+                                    <label className="text-sm sm:text-base font-medium md:col-span-1">
+                                        Добавить пользователей к текущей подписке
+                                    </label>
+                                    <input
+                                        type="number"
+                                        min="1"
+                                        placeholder="Количество пользователей"
+                                        className="mt-1 p-2 bg-white border border-black rounded w-full md:col-span-2"
+                                    />
+                                </div>
+                                <div className="flex flex-wrap justify-between items-center gap-4 mt-6">
+                                    <p className="text-lg sm:text-xl font-bold">Сумма: ??? Р</p>
+                                    <button
+                                        // onClick={...}
+                                        className="bg-[#092155] text-white py-2 px-4 rounded hover:bg-[#2a4992] active:bg-[#dadee7] font-semibold active:text-black transition-all duration-150 ease-in-out transform active:scale-95 w-full sm:w-auto">
+                                        Оплатить
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 )}
-                <h1 className="text-2xl sm:text-3xl font-bold text-black mb-4 sm:mb-6">
-                    Выберите подходящий тариф
-                </h1>
-                <p className="text-black text-base sm:text-lg mb-10">
-                    Гибкие тарифные планы для любого бизнеса. Экономьте с увеличением команды
-                </p>
-
-                <div className="flex flex-col sm:flex-row flex-wrap gap-6 sm:gap-8 justify-center items-stretch">
-                    {plans.map((plan, index) => (
-                        <div
-                            key={index}
-                            className="bg-white border-2 border-black rounded-lg shadow-[14px_14px_15px_rgba(0,0,0,0.32)] px-6 py-8 w-full sm:w-[90%] md:w-[420px] flex flex-col justify-between mx-auto"
-                        >
-                            <div>
-                                <h2 className="text-lg sm:text-xl font-extrabold text-black mb-1">
-                                    {plan.title}
-                                </h2>
-                                <p className="text-black font-medium mb-4">{plan.subtitle}</p>
-
-                                {plan.durationInput}
-
-                                <p className="text-xl sm:text-2xl font-bold text-black mb-4">
-                                    {plan.price}
-                                </p>
-
-                                <ul className="mb-6 space-y-2 text-black font-medium">
-                                    {plan.features.map((feature, i) => (
-                                        <li key={i}>• {feature}</li>
-                                    ))}
-                                </ul>
-                            </div>
-                            <button
-                                onClick={plan.onClick}
-                                className="mt-auto bg-[#092155] text-white py-2 px-4 rounded hover:bg-[#2a4992] active:bg-[#dadee7] font-semibold active:text-black transition-all duration-150 ease-in-out transform active:scale-95">
-                                {plan.button}
-                            </button>
-                        </div>
-                    ))}
-                </div>
             </div>
         </div>
     );
