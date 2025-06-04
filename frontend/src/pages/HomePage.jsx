d8effc9f4b657dce29e822f65400b298ba563d34
C0r1e0ne <sahasid89@mail.ru>
2025-06-04 11:52:55 +0300

    hotfix

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -8,42 +8,26 @@ export const HomePage = () => {
     const email = user?.email || null;
 
     useEffect(() => {
-        if (!document.getElementById("yandex-metrika")) {
+        if (!window.ym) {
             const script = document.createElement("script");
             script.id = "yandex-metrika";
-            script.type = "text/javascript";
-            script.innerHTML = `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) { return; }
-            }
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],
-            k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(102385915, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true
-            });
-        `;
+            script.src = "https://mc.yandex.ru/metrika/tag.js";
+            script.async = true;
+            script.onload = () => {
+                window.ym && window.ym(102385915, "init", {
+                    clickmap: true,
+                    trackLinks: true,
+                    accurateTrackBounce: true,
+                });
+            };
             document.head.appendChild(script);
 
-            const noscript = document.createElement("noscript");
-            noscript.innerHTML = `<div><img src="https://mc.yandex.ru/watch/102385915" style="position:absolute; left:-9999px;" alt="" /></div>`;
-            document.body.appendChild(noscript);
+            const noscript = document.createElement("div");
+            noscript.innerHTML = `<img src="https://mc.yandex.ru/watch/102385915" style="position:absolute; left:-9999px;" alt="" />`;
+            const container = document.createElement("noscript");
+            container.appendChild(noscript);
+            document.body.appendChild(container);
         }
-
-        // // DialogX widget
-        // if (!document.getElementById("dialogx-widget")) {
-        //     const widgetScript = document.createElement("script");
-        //     widgetScript.id = "dialogx-widget";
-        //     widgetScript.src = "https://dialogx.ru/widget.js";
-        //     widgetScript.async = true;
-        //     widgetScript.dataset.widgetToken = "demo-token";
-        //     document.body.appendChild(widgetScript);
-        // }
     }, []);
     return (
         <div className="bg-[#E6E5EA]">
