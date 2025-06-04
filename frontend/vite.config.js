09d44b7c124c5e6ab0c7123a6c8fe6a962fa3f8c
C0r1e0ne <sahasid89@mail.ru>
2025-06-04 03:15:58 +0300

    HOTFIX

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite"
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
   server: {
     host: '0.0.0.0',
     port: 5173,
     origin: 'http://dialogx.ru',
     cors: true,
-  },
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, 'src/components/embed.jsx'),
-      name: 'ChatWidget',
-      fileName: () => 'widget.js',
-      formats: ['iife'],
-    },
-    rollupOptions: {
-      external: ['react', 'react-dom'],
-      output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-        assetFileNames: 'index.css',
-      },
-    },
-    outDir: 'dist',
-    emptyOutDir: true,
-  },
-});
+  }
+})
